1bd38195f4e565c83b775fe9adeaacc758a9ab75
Mitchell Cleveland <mitchellclev@outlook.com>
2021-08-13 12:14:24 -0500

    Scan comp halfway complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.scss";
 import Rewards from "./pages/Rewards/Rewards";
+import SecretMenu from "./components/SecretMenu/SecretMenu";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Suggested from "./components/Suggested/Suggested";
 
 function App() {
   return (
