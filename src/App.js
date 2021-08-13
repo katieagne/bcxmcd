219a510d60820baa3bf9a476023274c6ddf81dff
katieagne <katieagne@gmail.com>
2021-08-13 00:39:23 -0700

    create suggested section component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
+import Suggested from "./components/Suggested/Suggested";
 
 function App() {
   return (
     <div className="App">
+      <Suggested />
       <Footer />
     </div>
   );
