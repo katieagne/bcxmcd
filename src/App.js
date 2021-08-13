26dddfe8fe92cdfa2a41e28daf908840149ac28f
Mitchell Cleveland <mitchellclev@outlook.com>
2021-08-13 00:28:29 -0500

    Navbar structure created, waiting on assetts from UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>hellooooooo</h1>
     </div>
   );
