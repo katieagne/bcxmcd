a72e1f01decfab0ec7b76fd9d7bbd9c6dc363171
katieagne <katieagne@gmail.com>
2021-08-13 00:16:41 -0700

    build footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "./footer.scss";
+import facebook from "../../assets/icons/facebook.svg";
+import twitter from "../../assets/icons/twitter.svg";
+import youtube from "../../assets/icons/youtube.svg";
+import snapchat from "../../assets/icons/snapchat.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import pinterest from "../../assets/icons/pinterest.svg";
+import app from "../../assets/icons/app-store.png";
+import play from "../../assets/icons/play-store.png";
 
 const Footer = () => {
   return (
@@ -15,9 +23,19 @@ const Footer = () => {
         </div>
         <div className="footer__socials-container">
           <div className="footer__socials">
-            <div className="footer__social-icons"></div>
+            <div className="footer__social-icons">
+              <img className="footer__icon" src={facebook} />
+              <img className="footer__icon" src={twitter} />
+              <img className="footer__icon" src={youtube} />
+              <img className="footer__icon" src={snapchat} />
+              <img className="footer__icon" src={instagram} />
+              <img className="footer__icon" src={pinterest} />
+            </div>
           </div>
-          <div className="footer__app"></div>
+          <div className="footer__app">
+            <img className="footer__icon" src={app} />
+            <img className="footer__icon--zero" src={play} />
+          </div>
         </div>
       </div>
     </section>
